538aad0dcf12852802c78b31600f6599c44bee1f
SantinoRusso <santino.russo04@gmail.com>
2023-05-30 13:25:35 -0300

    cambios

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,42 +3,42 @@ import '../assets/css/app.css';
 import img from "../assets/images/logo-DH.png"
 
 
-function SideBar() {
-  return (
-    <div>
+function SideBar() {                                        
+   return (
+     <div>
       <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-        <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+         <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
           <div className="sidebar-brand-icon">
-            <img className="w-100" src={img} alt="Digital House"/>
-          </div>
-        </a>
+             <img className="w-100" src={img} alt="Digital House"/>
+           </div>
+         </a>
         
-        <hr className="sidebar-divider my-0"/>
+         <hr className="sidebar-divider my-0"/>
 
-        <li className="nav-item active">
-          <a className="nav-link" href="/">
-            <i className="fas fa-fw fa-tachometer-alt"></i>
+         <li className="nav-item active">
+           <a className="nav-link" href="/">
+             <i className="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard - DH Tik-y-yura</span></a>
         </li>
-        
+         
         <hr className="sidebar-divider"/>
-        
+         
         <div className="sidebar-heading">Actions</div>
-        
+         
         <li className="nav-item">
           <a className="nav-link collapsed" href="/">
             <i className="fas fa-fw fa-folder"></i>
             <span>Pages</span>
           </a>
         </li>
-        
+         
         <li className="nav-item">
-          <a className="nav-link" href="/">
-            <i className="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
+             <a className="nav-link" href="/">
+             <i className="fas fa-fw fa-chart-area"></i>
+             <span>Charts</span></a>
         </li>
-
+ 
         <li className="nav-item">
           <a className="nav-link" href="/">
             <i className="fas fa-fw fa-table"></i>
